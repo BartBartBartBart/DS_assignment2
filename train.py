e9440bb0d8e86e4dcdded19e01643fe885700f8d
Lisanne <l.t.wallaard@umail.leidenuniv.nl>
2023-05-14 14:54:49 +0200

    rmse

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import torch
+# import torch
     
 def load_data():
     train_df = pd.read_csv("./data/train.csv",encoding="latin-1",on_bad_lines = 'warn')
@@ -28,11 +28,11 @@ def tokenize_data(tokenizer, data):
         "attention_mask": tokenized_data["attention_mask"]
     }
 
-def load_perceptron(input_shape=(768,)):
+def load_perceptron(input_shape):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(128, activation='relu', input_shape=input_shape))
     model.add(tf.keras.layers.Dense(1, activation='linear'))
-    model.compile(loss="rmse", optimizer="adam")
+    model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer="adam")
     return model 
 
 def train():
@@ -49,23 +49,41 @@ def train():
     X = dataset.drop(["product_info", "relevance"], axis=1)
     y = dataset.drop(["product_info", "token_type_ids", "input_ids", "attention_mask"], axis=1)
 
-    X['input_ids'] = X['input_ids'].map(lambda x: tf.convert_to_tensor(np.array(x)))
-    X['token_type_ids'] = X['token_type_ids'].map(lambda x: tf.convert_to_tensor(np.array(x)))
-    X['attention_mask'] = X['attention_mask'].map(lambda x: tf.convert_to_tensor(np.array(x)))
+    X['input_ids'] = X['input_ids'].map(lambda x: np.array(x))
+    X['token_type_ids'] = X['token_type_ids'].map(lambda x: np.array(x))
+    X['attention_mask'] = X['attention_mask'].map(lambda x: np.array(x))
 
     X_train, X_test, y_train, y_test = train_test_split(
          X, y, test_size=0.2, random_state=42
     )  
-
-    # X_train = tf.convert_to_tensor(X_train)
-    # X_test = tf.convert_to_tensor(X_test)
-    # y_train = tf.convert_to_tensor(y_train)
-    # y_test = tf.convert_to_tensor(y_test)
     
-    perceptron = load_perceptron()
-    perceptron.fit(X_train, y_train, epochs=20, batch_size=20)
+    # print(type(X_train["input_ids"].iloc[0]), type(X_test), type(y_train), type(y_test.iloc[0]))
+    # X_train = X_train.map(lambda x: np.array(x))
+
+    # # convert to list
+    # X_train = X_train.values.tolist()
+    # X_test = X_test.values.tolist()
+    # y_train = y_train.values.tolist()
+    # y_test = y_test.values.tolist()
+       
+    X_train = np.array(X_train.values.tolist())
+    X_test = np.array(X_test.values.tolist())
+    y_train = np.array(y_train.values.tolist())
+    y_test = np.array(y_test.values.tolist())
+
+       
+    # print(X_train.iloc[0])
+    
+    perceptron = load_perceptron(input_shape=(None, None, 3, 512))
+    perceptron.fit(X_train, y_train, epochs=20, batch_size=32, verbose=1)
     _, accuracy = perceptron.evaluate(X_test, y_test)
+    y_pred = perceptron.predict(X_test)
     print("ACCURACY ", accuracy)
+    
+    mse = mean_squared_error(y_test, y_pred)
+    # mae = mean_absolute_error(y_test, y_pred)
+
+    print(f"MSE: {mse}")
     
     # APPROACH
     # Tokenize the input query and product using BERT to generate fixed-length vector representations.
