cdd93ffdd9caff6e5a812586ba7df269304bde97
Bart <bart.denboef@gmail.com>
2023-05-16 13:14:12 +0200

    checked predictions

--- train.py
+++ train.py
@@ -29,9 +29,12 @@ def tokenize_data(tokenizer, data):
         "attention_mask": tokenized_data["attention_mask"]
     }
 
-def load_perceptron(input_shape):
+def load_nn(input_shape):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dense(64, activation='relu'))
+    model.add(tf.keras.layers.Dense(32, activation='relu'))
+    model.add(tf.keras.layers.Dense(32, activation='relu'))
     model.add(tf.keras.layers.Dense(1, activation='linear'))
     model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer="adam")
     return model 
@@ -49,10 +52,11 @@ def train():
     
     X = dataset.drop(["product_info", "relevance"], axis=1)
     y = dataset.drop(["product_info", "token_type_ids", "input_ids", "attention_mask"], axis=1)
+    # y = dataset.drop(["product_info", "token_type_ids"], axis=1)
 
-    X['input_ids'] = X['input_ids'].map(lambda x: np.array(x))
-    X['token_type_ids'] = X['token_type_ids'].map(lambda x: np.array(x))
-    X['attention_mask'] = X['attention_mask'].map(lambda x: np.array(x))
+    # X['input_ids'] = X['input_ids'].map(lambda x: np.array(x))
+    # X['token_type_ids'] = X['token_type_ids'].map(lambda x: np.array(x))
+    # X['attention_mask'] = X['attention_mask'].map(lambda x: np.array(x))
 
     X_train, X_test, y_train, y_test = train_test_split(
          X, y, test_size=0.2, random_state=42
@@ -63,17 +67,22 @@ def train():
     y_train = np.array(y_train.values.tolist())
     y_test = np.array(y_test.values.tolist())
 
-       
+    # print(X_test)
+    print(X_test[0])
+    print(X_test[1])
     
-    perceptron = load_perceptron(input_shape=(None, None, 3, 512))
-    perceptron.fit(X_train, y_train, epochs=20, batch_size=32, verbose=1)
+    perceptron = load_nn(input_shape=(None, None, 3, 512))
+    perceptron.fit(X_train, y_train, epochs=20, batch_size=16, verbose=1)
     y_pred = perceptron.predict(X_test)
+    
+    for test, pred in zip(y_test,y_pred):
+        print(f"Prediction {pred}, Truth {test}")
     
     # y_pred has shape [14814,3,1], we take 1st prediction (TODO: maybe reshape y_test or at least double check)
     # reshape into [14814,1] because that is shape of y_test 
     y_pred = y_pred[:,0,:]
     y_pred = np.reshape(y_pred, (14814,1))
-
+    
     mse = mean_squared_error(y_test, y_pred)
 
     rmse = math.sqrt(mse)
